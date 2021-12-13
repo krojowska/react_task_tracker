53dd0a58ee85ea1ea60e37213e8bfdc48514c147
Dzban <Dzban@example.com>
2021-12-13 16:20:32 +0100

    2nd commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
+const Header = ({title, onAdd, showAdd}) => {
 
-const Header = ({title}) => {
     return (
         <header className='header' >
             <h1>{title}</h1>
-            <button className='btn' >
-                Add
-            </button>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            
         </header>
     )
 }
